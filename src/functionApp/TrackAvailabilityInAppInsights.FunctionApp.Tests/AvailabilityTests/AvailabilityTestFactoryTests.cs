8f2e8203abc8b336261b426aed9677f1f4b7b910
Ronald Bosma <ronaldbosma@outlook.com>
2025-04-15 17:21:53 +0200

    Add tests for AvailabilityTestFactory

---
+++ src/functionApp/TrackAvailabilityInAppInsights.FunctionApp.Tests/AvailabilityTests/AvailabilityTestFactoryTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using TrackAvailabilityInAppInsights.FunctionApp.AvailabilityTests;
+using TrackAvailabilityInAppInsights.FunctionApp.Tests.Fakes;
+
+namespace TrackAvailabilityInAppInsights.FunctionApp.Tests.AvailabilityTests
+{
+    [TestClass]
+    public class AvailabilityTestFactoryTests
+    {
+        [TestMethod]
+        public void CreateAvailabilityTest_SpecifyTestNameAndCheckFunction_AvailabilityTestReturned()
+        {
+            //Arrange
+            var sut = new AvailabilityTestFactory(new TelemetryChannelFake().CreateTelemetryClient(), new HttpClientFactoryFake(), new NullLoggerFactory());
+
+            //Act
+            var result = sut.CreateAvailabilityTest("A Test Name", () => Task.CompletedTask);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType<AvailabilityTest>(result);
+        }
+
+        [TestMethod]
+        public void CreateAvailabilityTest_SpecifyTestNameAndRequestUrlAndClientName_HttpGetRequestAvailabilityTestReturned()
+        {
+            //Arrange
+            var sut = new AvailabilityTestFactory(new TelemetryChannelFake().CreateTelemetryClient(), new HttpClientFactoryFake(), new NullLoggerFactory());
+
+            //Act
+            var result = sut.CreateAvailabilityTest("A Test Name", "/health", "A Client Name");
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType<HttpGetRequestAvailabilityTest>(result);
+        }
+    }
+
+}
