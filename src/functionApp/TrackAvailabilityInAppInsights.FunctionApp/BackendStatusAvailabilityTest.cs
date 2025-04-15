422ac213592b7d5ad18fdaddd510aaa48be57f63
Ronald Bosma <ronaldbosma@outlook.com>
2025-04-15 18:00:23 +0200

    Use primary constructor

--- src/functionApp/TrackAvailabilityInAppInsights.FunctionApp/BackendStatusAvailabilityTest.cs
+++ src/functionApp/TrackAvailabilityInAppInsights.FunctionApp/BackendStatusAvailabilityTest.cs
@@ -6,25 +6,17 @@ namespace TrackAvailabilityInAppInsights.FunctionApp
     /// <summary>
     /// Function to test the availability of the Backend API.
     /// </summary>
-    public class BackendStatusAvailabilityTest
+    public class BackendStatusAvailabilityTest(IAvailabilityTestFactory availabilityTestFactory)
     {
-        private readonly IAvailabilityTestFactory _availabilityTestFactory;
-
-        public BackendStatusAvailabilityTest(IAvailabilityTestFactory availabilityTestFactory)
-        {
-            _availabilityTestFactory = availabilityTestFactory;
-        }
+        private const string TestName = "Azure Function - Backend API Status Test";
+        private const string RequestUri = "/backend/status";
+        private const string HttpClientName = "apim";
 
         [Function(nameof(BackendStatusAvailabilityTest))]
         public async Task Run([TimerTrigger("0 * * * * *")] TimerInfo timerInfo)
         {
-            const string TestName = "Azure Function - Backend API Status Test";
-            const string RequestUri = "/backend/status";
-            const string HttpClientName = "apim";
-
-            var availabilityTest = _availabilityTestFactory.CreateAvailabilityTest(TestName, RequestUri, HttpClientName);
+            var availabilityTest = availabilityTestFactory.CreateAvailabilityTest(TestName, RequestUri, HttpClientName);
             await availabilityTest.ExecuteAsync();
         }
     }
-
 }
