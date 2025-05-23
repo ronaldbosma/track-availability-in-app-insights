5bcf959052748afefdda851ab9544f6cd5501618
Ronald Bosma <ronaldbosma@outlook.com>
2025-05-23 14:11:23 +0200

    Use separate script for logicApp prepackage hook

---
+++ hooks/prepackage-logicapp-build-functions-project.ps1
@@ -0,0 +1,4 @@
+# The Logic App has a custom .NET code project. We need to build the .NET project first
+# in order for the assemblies to be included in the /lib/custom folder of the Logic App
+
+dotnet build ../Functions/TrackAvailabilityInAppInsights.LogicApp.Functions.csproj --configuration Release
