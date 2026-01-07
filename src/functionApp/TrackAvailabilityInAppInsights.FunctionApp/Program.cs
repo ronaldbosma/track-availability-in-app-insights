5aae80a2ffdf18ea22f1651cca901211657df746
Ronald Bosma <ronaldbosma@outlook.com>
2026-01-07 12:21:15 +0100

    Enable informational logging in function

--- src/functionApp/TrackAvailabilityInAppInsights.FunctionApp/Program.cs
+++ src/functionApp/TrackAvailabilityInAppInsights.FunctionApp/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using TrackAvailabilityInAppInsights.FunctionApp;
 
 var builder = FunctionsApplication.CreateBuilder(args);
@@ -7,5 +9,18 @@ var builder = FunctionsApplication.CreateBuilder(args);
 builder.ConfigureFunctionsWebApplication();
 
 builder.Services.RegisterDependencies(builder.Configuration);
+
+builder.Logging.Services.Configure<LoggerFilterOptions>(options =>
+{
+    // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
+    // Log levels can also be configured using appsettings.json. For more information, see https://learn.microsoft.com/azure/azure-monitor/app/worker-service#ilogger-logs
+    LoggerFilterRule? defaultRule = options.Rules?.FirstOrDefault(rule => rule.ProviderName
+        == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+    if (defaultRule is not null && options.Rules is not null)
+    {
+        options.Rules.Remove(defaultRule);
+    }
+});
 
 builder.Build().Run();
