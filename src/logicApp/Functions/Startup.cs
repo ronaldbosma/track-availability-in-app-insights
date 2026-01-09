b7724fc175398890a1346daef243be66f6ddce01
Ronald Bosma <ronaldbosma@outlook.com>
2026-01-09 15:58:33 +0100

    Inject TelemetryClient into logic app functions class

---
+++ src/logicApp/Functions/Startup.cs
@@ -0,0 +1,29 @@
+namespace TrackAvailabilityInAppInsights.LogicApp.Functions
+{
+    using System;
+    using Microsoft.Azure.Functions.Extensions.Workflows;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.ApplicationInsights;
+    using Microsoft.ApplicationInsights.Channel;
+    using Microsoft.ApplicationInsights.Extensibility;
+
+    public class Startup : IConfigureStartup
+    {
+        /// <summary>
+        /// Configures services for the Logic App custom .NET code project.
+        /// </summary>
+        /// <param name="services">The service collection to configure.</param>
+        public void Configure(IServiceCollection services)
+        {
+            
+            TelemetryConfiguration telemetryConfiguration = new()
+            {
+                ConnectionString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING"),
+                TelemetryChannel = new InMemoryChannel()
+            };
+            TelemetryClient telemetryClient = new(telemetryConfiguration); 
+
+            services.AddSingleton(telemetryClient);
+        }
+    }
+}
