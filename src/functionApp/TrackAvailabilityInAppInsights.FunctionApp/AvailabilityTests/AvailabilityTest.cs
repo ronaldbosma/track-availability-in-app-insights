2d1ac56c8e04e369f0e35f14f7ed5ff84aa987be
Ronald Bosma <ronaldbosma@outlook.com>
2026-01-09 15:45:34 +0100

    Set timestamp when creating AvailabilityTelemetry

--- src/functionApp/TrackAvailabilityInAppInsights.FunctionApp/AvailabilityTests/AvailabilityTest.cs
+++ src/functionApp/TrackAvailabilityInAppInsights.FunctionApp/AvailabilityTests/AvailabilityTest.cs
@@ -19,7 +19,9 @@ namespace TrackAvailabilityInAppInsights.FunctionApp.AvailabilityTests
             {
                 Name = name,
                 RunLocation = Environment.GetEnvironmentVariable("REGION_NAME") ?? "Unknown",
-                Success = false
+                Success = false,
+                // Sets the start time of availability test
+                Timestamp = DateTimeOffset.UtcNow
             };
 
             Stopwatch stopwatch = new();
@@ -36,9 +38,6 @@ namespace TrackAvailabilityInAppInsights.FunctionApp.AvailabilityTests
                     availability.Id = activity.SpanId.ToString();
                     availability.Context.Operation.ParentId = activity.ParentSpanId.ToString();
                     availability.Context.Operation.Id = activity.RootId;
-
-                    // Set start time of availability test
-                    availability.Timestamp = DateTimeOffset.UtcNow;
 
                     await checkAvailabilityAsync();
                 }
