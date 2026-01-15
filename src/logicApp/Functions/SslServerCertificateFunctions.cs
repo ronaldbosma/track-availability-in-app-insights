28b0d0a28482b6f5169fa10add3485ceaca430f3
Ronald Bosma <ronaldbosma@outlook.com>
2026-01-15 15:06:55 +0100

    Add some comments to GetSslServerCertificateExpirationInDays function

--- src/logicApp/Functions/SslServerCertificateFunctions.cs
+++ src/logicApp/Functions/SslServerCertificateFunctions.cs
@@ -26,19 +26,25 @@ namespace TrackAvailabilityInAppInsights.LogicApp.Functions
         {
             _logger.LogInformation("GetSslServerCertificateExpirationInDays function invoked with hostname: {Hostname}, port: {Port}", hostname, port);
             
+            // Connect client to remote TCP host using provided hostname and port
             using var tcpClient = new TcpClient();
             await tcpClient.ConnectAsync(hostname, port);
 
+            // Create an SSL stream over the TCP connection and authenticate with the server
+            // This will trigger the SSL/TLS handshake and allow us to access the server's certificate
             using var sslStream = new SslStream(tcpClient.GetStream());
             await sslStream.AuthenticateAsClientAsync(hostname);
 
+            // Retrieve the remote server's SSL certificate from the authenticated connection
             var certificate = sslStream.RemoteCertificate;
             if (certificate != null)
             {
+                // Calculate the remaining lifetime of the certificate in days
                 var x509Certificate = new X509Certificate2(certificate);
                 return (x509Certificate.NotAfter - DateTime.UtcNow).Days;
             }
 
+            // Throw an exception if no certificate was found
             throw new Exception($"No SSL server certificate found for host {hostname} on port {port}. Unable to determine expiration.");
         }
     }
