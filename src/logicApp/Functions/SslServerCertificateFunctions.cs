afd52fcf181812acfc6254421cff7f4ebfb7de25
Ronald Bosma <ronaldbosma@outlook.com>
2026-01-15 15:03:31 +0100

    Add workflow to check APIM's SSL server certificate

---
+++ src/logicApp/Functions/SslServerCertificateFunctions.cs
@@ -0,0 +1,45 @@
+namespace TrackAvailabilityInAppInsights.LogicApp.Functions
+{
+    using System;
+    using System.Net.Security;
+    using System.Net.Sockets;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Functions.Extensions.Workflows;
+    using Microsoft.Azure.Functions.Worker;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Contains functions related to SSL server certificates.
+    /// </summary>
+    public class SslServerCertificateFunctions
+    {
+        private readonly ILogger<SslServerCertificateFunctions> _logger;
+
+        public SslServerCertificateFunctions(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<SslServerCertificateFunctions>();
+        }
+
+        [Function("GetSslServerCertificateExpirationInDays")]
+        public async Task<int> GetSslServerCertificateExpirationInDays([WorkflowActionTrigger] string hostname, int port)
+        {
+            _logger.LogInformation("GetSslServerCertificateExpirationInDays function invoked with hostname: {Hostname}, port: {Port}", hostname, port);
+            
+            using var tcpClient = new TcpClient();
+            await tcpClient.ConnectAsync(hostname, port);
+
+            using var sslStream = new SslStream(tcpClient.GetStream());
+            await sslStream.AuthenticateAsClientAsync(hostname);
+
+            var certificate = sslStream.RemoteCertificate;
+            if (certificate != null)
+            {
+                var x509Certificate = new X509Certificate2(certificate);
+                return (x509Certificate.NotAfter - DateTime.UtcNow).Days;
+            }
+
+            throw new Exception($"No SSL server certificate found for host {hostname} on port {port}. Unable to determine expiration.");
+        }
+    }
+}
