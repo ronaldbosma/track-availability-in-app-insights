ee751937adb70e8128838c0b3351f45ce485484b
Ronald Bosma <ronaldbosma@outlook.com>
2026-03-27 13:35:27 +0100

    Combine Stopwatch creation and start

--- src/functionApp/TrackAvailabilityInAppInsights.FunctionApp/AvailabilityTests/AvailabilityTest.cs
+++ src/functionApp/TrackAvailabilityInAppInsights.FunctionApp/AvailabilityTests/AvailabilityTest.cs
@@ -25,8 +25,7 @@ namespace TrackAvailabilityInAppInsights.FunctionApp.AvailabilityTests
                 Timestamp = DateTimeOffset.UtcNow
             };
 
-            Stopwatch stopwatch = new();
-            stopwatch.Start();
+            var stopwatch = Stopwatch.StartNew();
 
             try
             {
@@ -37,7 +36,7 @@ namespace TrackAvailabilityInAppInsights.FunctionApp.AvailabilityTests
 
                     // Connect the availability telemetry to the logging activity
                     availability.Id = activity.SpanId.ToString();
-                        
+
                     await checkAvailabilityAsync();
                 }
 
